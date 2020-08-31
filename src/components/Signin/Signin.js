aeeab7d66cc23a44ad3edd1f453460352308d6d5
Zaki Afada <afadazaki32@gmail.com>
2020-08-31 10:02:57 +0700

    first-deploy

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('https://mighty-mesa-24387.herokuapp.com/signin', {
+    fetch('http://localhost:3001/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
